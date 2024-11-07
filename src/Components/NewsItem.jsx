c66ac80d22f722c63ecd62207852472ed5aedcd9
Vikas Chaurasia <vikascc28@gmail.com>
2024-11-08 01:26:33 +0530

    Completion of NewsViews Project

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,24 @@
+import image from '../assets/news.png';
+import PropTypes from 'prop-types';
+
+const NewsItem = ({title,description,src,url}) => {
+  return (
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 py-2 px-2" style={{maxWidth:"345px"}}>
+  <img src={src?src:image} style={{height:"200px", width:"327px"}} className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description?description.slice(0,90):"News isn't available."}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
+  </div>
+</div>
+  );
+};
+
+NewsItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  src: PropTypes.string,
+  url: PropTypes.string
+};
+
+export default NewsItem;
